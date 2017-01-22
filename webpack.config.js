46609e85a3f4a67ff6f6c9c3ecc1345e409439b8
Jeff Walters <caballero@me.com>
2017-01-22 15:55:36 -0600

    tweaked webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
-};
+}
 
 module.exports = {
   // Entry accepts a path or an object of entries.
@@ -19,40 +19,43 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack demo'
+      title: 'React Challenge'
     })
   ],
-    module: {
-      preloaders: [
-            {
-              test: /\.jsx?$/,
-              exclude: /node_modules/,
-              loader: "eslint-loader"
-            }
-      ],
-      loaders: [
-          {
-              test: /\.jsx?$/,
-              exclude: /node_modules/,
-              loader: 'babel-loader',
-              //query: {
-              //    presets: ['react', 'es2015', 'stage-2']
-              //}
-          }
-        ]
-    },
+  module: {
+    preloaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        //query: {
+        //    presets: ['react', 'es2015', 'stage-2']
+        //}
+      }
+    ]
+  },
 
-    resolve: {
-      root: [
-        path.resolve(__dirname, 'app'),
-        path.resolve(__dirname, 'node_modules')
-      ],
-      modules: [path.resolve(__dirname, "app"), "node_modules"],
-      extensions: ['', '.js', '.jsx', '.json']
-    },
-    stats: {
-      color: true,
-      reason: true,
-      chunks: false
-    }
-};
+  resolve: {
+    root: [
+      path.resolve(__dirname, 'app'),
+      path.resolve(__dirname, 'node_modules')
+    ],
+    modules: [
+      path.resolve(__dirname, "app"),
+      "node_modules"
+    ],
+    extensions: ['', '.js', '.jsx', '.json']
+  },
+  stats: {
+    color: true,
+    reason: true,
+    chunks: false
+  }
+}
